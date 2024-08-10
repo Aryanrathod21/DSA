004bca2feecf468c48d0a00de1e38ce01e590ce2
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-10 16:15:48 +0530

    array/basic

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 95; //maths
+        marks[1] = 93; //chemistry
+        marks[2] = 92; //Physics
+
+        for(int i = 0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
