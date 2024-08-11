9462db4a9330b95c5c72d02e6249de517b55eb76
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-11 22:50:12 +0530

    SelectionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,6 @@
 //Time Complexity : O(n^2)
 
-public class BubbleSort {
+public class SelectionSort {
     public static void printArray(int arr[]){
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
@@ -9,14 +9,15 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {2,8,7,4,9};
         for(int i = 0 ; i < arr.length-1;i++){
-            for(int j = 0;j < arr.length - i - 1;j++){
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp; 
+            int smallest = i;
+            for(int j = i+1;j < arr.length;j++){
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
                 }
-                
             }
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
         }
         printArray(arr);
     }
