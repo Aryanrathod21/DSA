9762ad356865d9e5ec6d41f26d9033f43d4034d9
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-15 00:01:30 +0530

    Recursion/reverse number

---
+++ RevRecursion.java
@@ -0,0 +1,14 @@
+public class RevRecursion {
+    public static void printRev(String str,int idx){
+        if ( idx == 0 ) {
+            System.out.print(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        printRev(str, idx-1);
+    }
+    public static void main(String[] args) {
+        String str = "hello";
+        printRev(str, str.length()-1);
+    }
+}
