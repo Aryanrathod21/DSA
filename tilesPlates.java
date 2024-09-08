59336a7022b225cbd6ef3d45774175bc52a63635
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-08 19:19:20 +0530

    Recursion/place tiles

---
+++ tilesPlates.java
@@ -0,0 +1,25 @@
+public class tilesPlates {
+
+    public static int placeTiles(int n,int m){
+
+        if (n == m) {
+            return 2;
+        }
+
+        if (n < m) {
+            return 1;
+        }
+
+        int vertical = placeTiles(n-m, m);
+
+        int horizontal = placeTiles(n-1, m);
+
+        return vertical + horizontal;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 2;
+        int answer = placeTiles(n, m);
+        System.out.println(answer);
+    }
+}
