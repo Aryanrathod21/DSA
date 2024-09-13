a9d93ff0fb72f8a26bc947f7cba2d9677f53cfc0
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-13 15:11:17 +0530

    OOPS constructor

---
+++ OOPS1.java
@@ -0,0 +1,24 @@
+class Student {
+
+    String name;
+    int age;
+
+    public void printDetails(){
+        System.out.println("Person name is : "+this.name);
+        System.out.println("Person age is : "+this.age);
+    }
+
+    Student(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
+
+public class OOPS1 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Aryan",20);
+        s1.printDetails();
+    }
+}
