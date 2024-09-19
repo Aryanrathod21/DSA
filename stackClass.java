ce87d2517bf723f0b590a3c6564bd9445d37d1bb
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-19 19:34:59 +0530

    Stack problem using linkedlist

--- stackClass.java
+++ stackClass.java
@@ -1,3 +1,6 @@
+
+import java.util.Stack;
+
 public class stackClass {
     static class Node{
         int data;
@@ -44,6 +47,15 @@ public class stackClass {
         }
     }
     public static void main(String[] args) {
-        
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
