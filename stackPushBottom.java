ac576d4a028a8b345e21e9d99891de8fc1672e48
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-19 20:27:59 +0530

    Stack problem push at bottom/reverse

--- stackPushBottom.java
+++ stackPushBottom.java
@@ -14,12 +14,25 @@ public class stackPushBottom {
         s.push(top);
     }
 
+    public static void reverse(Stack<Integer> s){
+        if (s.isEmpty()) {
+            return; 
+         }
+ 
+         int top = s.pop();
+         reverse(s);
+         pushAtBottom(top, s);
+
+
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
         pushAtBottom(4, s);
+        reverse(s);
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
