cb242e5caa3bc37f3ee7bfa82adc0bd6e6e51cd9
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-21 19:16:04 +0530

    Queue implimentation Linked List logic

---
+++ QueueLL.java
@@ -0,0 +1,77 @@
+public class QueueLL {
+    static  class Node{
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+        
+    }
+    static class Queue{
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty(){
+            return head = null && tail = null;
+        }
+
+        public static boolean isFull(){
+            return (rear+1)%size == front;
+        }
+
+        //enqueue
+        public static void add(int data){
+            if (isFull()) {
+                System.out.println("Queue is full");
+                return;
+            }
+
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear+1) % size;
+            arr[rear] = data;
+        }
+
+        //dequeue
+        public static int remove(){
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if (front == rear) {
+                front = rear = -1;
+            }
+            else{
+                front = (front+1) % size;
+            }
+            return result;
+        }
+
+        //front
+        public static int peek(){
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
