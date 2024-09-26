cd9abd79c7e529c541485f974f7edbb0da35b997
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-26 19:13:00 +0530

    BinaryTree sum of total nodes

--- BinaryTreeOrderTravel.java
+++ BinaryTreeOrderTravel.java
@@ -95,10 +95,19 @@ public class BinaryTreeOrderTravel {
         return leftNode + rightNode + 1;        
     }
 
+    public static int sumOfNodes(Node root){
+        if (root == null) {
+            return 0;
+        }
+        int leftNode = sumOfNodes(root.left);
+        int rightNode = sumOfNodes(root.right);
+        return leftNode + rightNode + root.data;        
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(countNodes(root));
+        System.out.println(sumOfNodes(root));
     }
 }
