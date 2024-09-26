eaf36177229d98c77f8ea2817e4caf6512cd1ce0
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-09-26 19:08:39 +0530

    BinaryTree count total nodes

--- BinaryTreeOrderTravel.java
+++ BinaryTreeOrderTravel.java
@@ -85,13 +85,20 @@ public class BinaryTreeOrderTravel {
             }
             
         }
-
+    }
+    public static int countNodes(Node root){
+        if (root == null) {
+            return 0;
+        }
+        int leftNode = countNodes(root.left);
+        int rightNode = countNodes(root.right);
+        return leftNode + rightNode + 1;        
     }
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        levelOrder(root);
+        System.out.println(countNodes(root));
     }
 }
